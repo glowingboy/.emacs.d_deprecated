a27416295ef59db69f83dd8c809afe86a75d0c07
glowingboy <glowingboy@outlook.com>
2017-06-02 10:42:18 +0800

    remove company backend company-lua

--- fs/fs-lua.el
+++ fs/fs-lua.el
@@ -1,11 +1,14 @@
 (provide 'fs-lua)
 (require 'company)
 (require 'company-lua)
-(add-to-list 'company-backends 'company-lua)
 
-(add-hook 'lua-mode-hook (lambda ()
-			   (
-			    setq-local company-backends '(company-lua)
-				       )))
+
+
+;;(add-to-list 'company-backends 'company-lua)
+
+;;(add-hook 'lua-mode-hook (lambda ()
+			   ;; (
+			   ;;  setq-local company-backends '(company-lua)
+			   ;; 	       )))
 
 
