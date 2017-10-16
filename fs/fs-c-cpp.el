8c833e3474d976a9bdad6e08dea7472fc1ca50af
glowingboy <glowingboy@outlook.com>
2017-10-16 10:14:33 +0800

    back to rtags

--- fs/fs-c-cpp.el
+++ fs/fs-c-cpp.el
@@ -48,29 +48,28 @@
 ;;(setq company-clang-arguments '("-I/usr/include"))
 
 ;;irony
-(require 'irony)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(defun my-irony-mode-hook()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;; (require 'irony)
+;; (add-hook 'c++-mode-hook 'irony-mode)
+;; (add-hook 'c-mode-hook 'irony-mode)
+;; (defun my-irony-mode-hook()
+;;   (define-key irony-mode-map [remap completion-at-point]
+;;     'irony-completion-at-point-async)
+;;   (define-key irony-mode-map [remap complete-symbol]
+;;     'irony-completion-at-point-async))
+;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 
 ;;company-irony
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;;(setq company-backends (delete 'company-semantic company-backends))
-(eval-after-load 'company
- '(add-to-list 'company-backends 'company-irony))
+;; (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+;; (eval-after-load 'company
+;;  '(add-to-list 'company-backends 'company-irony))
 
 ;;company-irony-c-headers
-(require 'company-irony-c-headers)
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends '(company-irony-c-headers company-irony)))
+;; (require 'company-irony-c-headers)
+;; (eval-after-load 'company
+;;   '(add-to-list
+;;     'company-backends '(company-irony-c-headers company-irony)))
 
 ;;flycheck
 (require 'flycheck)
@@ -82,8 +81,8 @@
 				 )))
 
 ;;flycheck-irony
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+;; (eval-after-load 'flycheck
+;;   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 
 ;;company-c-headers
